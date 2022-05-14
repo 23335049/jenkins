d1f20ff223a39dc51dd923b8c7d94cb85c40a1f4
王雄飞 <wangxiongfei@xuebangsoft.com>
2022-05-14 14:28:01 +0800

    # modify

--- dep_install.groovy
+++ dep_install.groovy
@@ -1,27 +1,27 @@
 pipeline {
     agent any
     parameters {
-        string(name: 'VERSION', defaultValue: 'xxxxx', description: '版本')
-        choice(name: 'ARCH', choices: ['X86', 'ARM'], description: 'CPU架构')
-        choice(name: 'MODE', choices: ['SINGLE', 'CLUSTER'], description: '组网模式')
+        string(name: 'VERSION', defaultValue: 'xxxxx', description: 'pkg version')
+        choice(name: 'ARCH', choices: ['X86', 'ARM'], description: 'CPU ARCH')
+        choice(name: 'MODE', choices: ['SINGLE', 'CLUSTER'], description: 'HOST MODE')
     }
     stages {
         stage("upload install pkg") {
             steps {
                 sleep 5
-                echo "done"
+                echo "upload ${VERSION}: ${ARCH}_${MODE} pkg success"
             }
         }
         stage("uninstall") {
             steps {
                 sleep 5
-                echo "done"
+                echo "uninstall before pkg success"
             }
         }
         stage("install") {
             steps {
                 sleep 5
-                echo "done"
+                echo "install ${VERSION}: ${ARCH}_${MODE} pkg success"
             }
         }
     }
